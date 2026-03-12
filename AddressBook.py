7a7d334bb4ea2ea914b1066bba88eecf7fcec87c
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-03-12 11:55:47 +0530

    Add code to add user from address book class in address book system

--- AddressBook.py
+++ AddressBook.py
@@ -1,15 +1,35 @@
 class AddressBook:
+
+    def __init__(self):
+        self.contacts=[]
+
     def start(self):
         print("Welcome to Address Book program")
 
+        first_name=input("Enter first name : ")
+        last_name=input("Enter last name : ")
+        address=input("Enter address : ")
+        city=input("Enter city : ")
+        state=input("Enter state : ")
+        zip_code=input("Enter Zip : ")
+        phone_number=input("Enter phone number : ")
+        email=input("Enter email : ")
+        
+        contact= Contact(first_name,last_name,address,city,state,zip_code,phone_number,email)
+        self.contacts.append(contact)
+        print("\nContact Added Successfully")
+
+        for c in self.contacts:
+            c.display()
+
 class Contact:
-    def __init__(self,first_name,last_name,address,city,state,zip,phone_number,email):
+    def __init__(self,first_name,last_name,address,city,state,zip_code,phone_number,email):
         self.first_name=first_name
         self.last_name=last_name
         self.address=address
         self.city=city
         self.state=state
-        self.zip=zip
+        self.zip_code=zip_code
         self.phone_number=phone_number
         self.email=email
     
@@ -19,21 +39,10 @@ class Contact:
         print("Address : ",self.address)
         print("City : ",self.city)
         print("State: ",self.state)
-        print("Zip : ",self.zip)
+        print("Zip : ",self.zip_code)
         print("Phone number : ",self.phone_number)
         print("Email : ",self.email)
 
 obj = AddressBook()
 obj.start()
 
-c1= Contact(
-    "Tanya",
-    "Maheshwari",
-    "Xyz Road",
-    "Abc",
-    "Uttar Pradesh",
-    "202001",
-    "8067890421",
-    "xyz@gmail.com"
-)
-c1.display()
