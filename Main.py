5668e6225d51087f2e54e204ac3087a2a9205de8
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-03-13 10:45:55 +0530

    Add multiple user functionality

--- Main.py
+++ Main.py
@@ -1,31 +1,39 @@
-from Contact import Contact
-from Operations.edit import edit_user
-from AddressBook import  AddressBook
-from Operations.delete import delete_user
+from contact import Contact
+
+from addressBook import  AddressBook
+
+
 
 ab=  AddressBook()
 
 def start():
         print("Welcome to Address Book program")
 
-        first_name=input("Enter first name : ")
-        last_name=input("Enter last name : ")
-        address=input("Enter address : ")
-        city=input("Enter city : ")
-        state=input("Enter state : ")
-        zip_code=input("Enter Zip : ")
-        phone_number=input("Enter phone number : ")
-        email=input("Enter email : ")
+        while True:
+            first_name=input("Enter first name : ")
+            last_name=input("Enter last name : ")
+            address=input("Enter address : ")
+            city=input("Enter city : ")
+            state=input("Enter state : ")
+            zip_code=input("Enter Zip : ")
+            phone_number=input("Enter phone number : ")
+            email=input("Enter email : ")
         
-        contact= Contact(first_name,last_name,address,city,state,zip_code,phone_number,email)
-        ab.add_contact(contact)
-        ab.display_contact()
+            contact= Contact(first_name,last_name,address,city,state,zip_code,phone_number,email)
+            
+            ab.add_contact(contact)
+            ab.display_contact()
+
+            choice = input("Do you want to add another user ? (y/n) : ")
+            if(choice.lower()!="y"):
+                break
+
 start()
 
 choice =  input("Do you want to update details ? (y/n):")
-if choice=="y" :
+if choice =="y" :
     edit_user(ab)
 
 choice = input ("Do you want to delete details ? (y/n):")
-if choice== "y":
+if choice == "y":
     delete_user(ab)
--- Operations/delete.py
+++
@@ -1,8 +0,0 @@
-def delete_user(AddressBook):
-    name= input("Enter name of user want to delete : ")
-    for d in AddressBook.contacts:
-        if d.first_name == name :
-            AddressBook.contacts.remove(d)
-            print("User deleted successfully")
-            return
-    print("User not found.")
